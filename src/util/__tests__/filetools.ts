e282432183f73f60fe3da95a34cb87a04581a959
Shroomy Shroom <sporeventexplosion@gmail.com>
2018-12-27 20:18:04 +0800

    Rewrite file tools

--- src/util/__tests__/filetools.ts
+++ src/util/__tests__/filetools.ts
@@ -1,13 +1,92 @@
 import { getParts, normalizeLines, normalizeSingleLine } from '../filetools';
 
-const toArray = <K, V>(map: Map<K, V>) => Array.from(map);
-
 describe('getParts', () => {
-  test('returns default section on empty input', () => {
-    expect(toArray(getParts(''))).toEqual([['$$default', '']]);
+  test('returns default part on empty input', () => {
+    expect(getParts('')).toEqual([['$$default', '']]);
   });
-  test('uses \\n for newlines', () => {
-    expect(toArray(getParts('test\ntest\n'))).toEqual([['$$default', '\ntest\n']]);
-    expect(toArray(getParts('test\r\ntest\r\n'))).toEqual([['$$default', '\ntest\n']]);
+  test('returns default part', () => {
+    expect(getParts('no part')).toEqual([['$$default', 'no part']]);
+    expect(getParts('(no) part')).toEqual([['$$default', '(no) part']]);
+    expect(getParts('\ntest')).toEqual([['$$default', '\ntest']]);
   });
-})
+  test('returns part starting on first line', () => {
+    expect(getParts('#(a)')).toEqual([['a', '']]);
+    expect(getParts('#(part) test test\n')).toEqual([['part', ' test test\n']]);
+  });
+  test('returns default part with other parts', () => {
+    expect(getParts('\n#(a)'))
+      .toEqual([['$$default', '\n'], ['a', '']]);
+
+    expect(getParts('\ntest\n\n#(part) test test\n'))
+      .toEqual([['$$default', '\ntest\n\n'], ['part', ' test test\n']]);
+
+    expect(getParts('\ntest\n\n#(first) test test\n#(second)more test'))
+      .toEqual([['$$default', '\ntest\n\n'], ['first', ' test test\n'], ['second', 'more test']]);
+  });
+  test('throws on empty part tag', () => {
+    expect(() => getParts('#() text')).toThrow();
+    expect(() => getParts('\n#() other text')).toThrow();
+    expect(() => getParts('#(valid)\n#(also-valid)\n#() other text')).toThrow();
+  });
+  test('throws on part tag with illegal characters', () => {
+    expect(() => getParts('#($$default) test')).toThrow();
+    expect(() => getParts('\n#($$default) test')).toThrow();
+    expect(() => getParts('#(#) text')).toThrow();
+    expect(() => getParts('\n#(*) other text')).toThrow();
+    expect(() => getParts('#()\n#(also-valid)\n#(in_valid) other text')).toThrow();
+  });
+  test('throws on unclosed part tag', () => {
+    expect(() => getParts('#(')).toThrow();
+    expect(() => getParts('#(\n')).toThrow();
+    expect(() => getParts('#(test')).toThrow();
+    expect(() => getParts('#(strings\n')).toThrow();
+    expect(() => getParts('#(test test\n)')).toThrow();
+    expect(() => getParts('#(test test\n#(valid))')).toThrow();
+
+    expect(() => getParts('\n#(')).toThrow();
+    expect(() => getParts('test\n#(\n')).toThrow();
+    expect(() => getParts('test\n#(first)\n#(test')).toThrow();
+    expect(() => getParts('test\n#(strings\n')).toThrow();
+    expect(() => getParts('test\n#(test test\n)')).toThrow();
+  });
+  test('preserves newlines', () => {
+    expect(getParts('\n\r\n')).toEqual([['$$default', '\n\r\n']]);
+    expect(getParts('test\ntest\n')).toEqual([['$$default', 'test\ntest\n']]);
+    expect(getParts('test\r\ntest\r\n')).toEqual([['$$default', 'test\r\ntest\r\n']]);
+  });
+  test('throws on duplicate part tag names', () => {
+    expect(() => getParts('#(valid1)\n#(valid2)\n#(valid1)')).toThrow();
+    expect(() => getParts('#(valid1)\n#(valid2)\n#(valid2)')).toThrow();
+  });
+});
+
+describe('normalizeLines', () => {
+  test('returns empty array on empty string', () => {
+    expect(normalizeLines('')).toEqual([]);
+  });
+  test('returns empty array on string with only whitespace', () => {
+    expect(normalizeLines('  \t \u2028 ')).toEqual([]);
+    expect(normalizeLines('\n\n\r\n')).toEqual([]);
+    expect(normalizeLines('\n \v\t \r\n \u205f \n \r\n ')).toEqual([]);
+  });
+  test('removes whitespace between lines', () => {
+    expect(normalizeLines('  \ta string\n   \u2006 \r\n \u2028 some more text\n \f '))
+      .toEqual(['a string', 'some more text']);
+    expect(normalizeLines('  \ta string\n \n  \u1680 \r\n \u2028 some more text\n\n\n \f '))
+      .toEqual(['a string', 'some more text']);
+  });
+});
+
+describe('normalizeSingleLine', () => {
+  test('returns string without whitespace', () => {
+    expect(normalizeSingleLine('')).toBe('');
+    expect(normalizeSingleLine('  \t \u2028 ')).toBe('');
+    expect(normalizeSingleLine('\n\n\r\n')).toBe('');
+    expect(normalizeSingleLine('\n \v\t \r\n \u205f \n \r\n ')).toBe('');
+
+    expect(normalizeSingleLine('  \ta string\n   \u2006 \r\n \u2028 some more text\n \f '))
+      .toBe('a stringsome more text');
+    expect(normalizeSingleLine('  \ta string\n \n  \u1680 \r\n \u2028 some more text\n\n\n \f '))
+      .toBe('a stringsome more text');
+  });
+});
--- src/util/filetools.ts
+++ src/util/filetools.ts
@@ -1,42 +1,52 @@
 import pinyin from './pinyin';
 
-const getParts = (input: string): Map<string, string> => {
-  const lines = input.split(/\r?\n/);
-  // maps iterate in insertion order :)
-  const sections: Map<string, number> = new Map();
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    if (line.substr(0, 2) === '#(') {
-      const sectionMatch = (/^#\([0-9a-z\-]+\)/i).exec(line);
-      if (sectionMatch == null) {
-        throw new Error(`Invalid section heading on line ${i + 1}`);
-      }
-      const sectionHead = sectionMatch[0];
-      const sectionName = sectionHead.substring(2, sectionHead.length - 1);
-      if (sections.has(sectionName)) {
-        throw new Error(`Duplicate section name "${ sectionName }" on line ${ i + 1 }`);
-      }
-      sections.set(sectionName, i);
+const getParts = (input: string): Array<[string, string]> => {
+  const partStartRegex = /\n#\(/g;
+  const partRegex = /\n#\([0-9a-z\-]+\)/gi;
+
+  // special measures to detect part at start of string
+
+  const firstPartStartRegex = /^#\(/g;
+  const firstPartRegex = /^#\([0-9a-z\-]+\)/gi;
+
+  const parts: Set<string> = new Set();
+  const ret: Array<[string, string]> = [];
+
+  let partName = '$$default';
+  let partStart = 0;
+
+  if (firstPartStartRegex.test(input)) {
+    const firstSection = firstPartRegex.exec(input);
+    if (firstSection == null) {
+      throw new Error('Invalid section tag at position 0');
     }
+    const match = firstSection[0];
+    partName = match.substring(2, match.length - 1);
+    parts.add(partName);
+    partStart = match.length;
   }
 
-  let prevName = '$$default';
-  let prevLine = 0;
-  const ret: Map<string, string> = new Map();
-  for (const [ name, line ] of sections) {
-    if (line === 0) {
-      prevName = name;
+  while (partStartRegex.test(input)) {
+    const section = partRegex.exec(input);
+    // taking into account leading \n
+    const start = partStartRegex.lastIndex - 2;
+    // check that start points match
+    if (section == null || start - 1 !== partRegex.lastIndex - section[0].length) {
+      throw new Error(`Invalid section tag at position ${start}`);
     }
-    if (line > prevLine) {
-      ret.set(prevName, lines.slice(prevLine, line).join('\n').substr(prevName.length + 3));
-      prevLine = line;
-      prevName = name;
+    ret.push([partName, input.substring(partStart, start)]);
+
+    const match = section[0];
+    partName = match.substring(3, match.length - 1);
+    if (parts.has(partName)) {
+      throw new Error(`Duplicate part name ${partName} at position ${start}`);
     }
+    parts.add(partName);
+    partStart = start + match.length - 1;
   }
-  // must be true, right?
-  if (lines.length > prevLine) {
-    ret.set(prevName, lines.slice(prevLine).join('\n').substr(prevName.length + 3));
-  }
+  // the last part
+  ret.push([partName, input.substring(partStart)]);
+
   return ret;
 };
 
